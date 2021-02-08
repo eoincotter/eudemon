9ba48c46ae2176acb10b27aabb207c69cd56d17b
[eoin cotter] <[eoin369@gmail.com]>
2021-02-08 21:06:17 +0000

    Add defusion exercises and questionnaire; add theory page

--- script.js
+++ script.js
@@ -22,3 +22,19 @@ function myFunction(x) {
   x.classList.toggle("change");
   document.querySelector('.header-right-dropdown').classList.toggle("change");
 }
+
+// Cognitive Fusion Questionnaire Calculator
+function calculateScore() {
+  var q1 = Number(document.forms["cognitive-fusion-questionnaire"]["q1"].value);
+  var q2 = Number(document.forms["cognitive-fusion-questionnaire"]["q2"].value);
+  var q3 = Number(document.forms["cognitive-fusion-questionnaire"]["q3"].value);
+  var q4 = Number(document.forms["cognitive-fusion-questionnaire"]["q4"].value);
+  var q5 = Number(document.forms["cognitive-fusion-questionnaire"]["q5"].value);
+  var q6 = Number(document.forms["cognitive-fusion-questionnaire"]["q6"].value);
+  var q7 = Number(document.forms["cognitive-fusion-questionnaire"]["q7"].value);
+  var totalScore = q1 + q2 + q3 + q4 + q5 + q6 + q7;
+  document.getElementById("CFQ-score-confirmation").innerHTML = `Your score: ${totalScore}`;
+  document.getElementById("CFQ-score-confirmation").style.display = "inline";
+  document.getElementById("CFQ-submit").style.display = "none";
+  return false;
+  }
