a3a185cbaec4b547232b0bdc89235fa9427b5e11
[eoin cotter] <[eoin369@gmail.com]>
2021-02-10 18:19:03 +0000

    Add acceptance page

--- script.js
+++ script.js
@@ -1,26 +1,7 @@
-let navOptions = document.querySelectorAll('li a');
-let dropdown = document.querySelector('.dropdown');
-
-let showDropdown = function() {
-    dropdown.style.display = 'block';    
-};
-
-let reset = function() {
-    dropdown.style.display = '';  
-};
-
-var i;
-for (i = 0; i < navOptions.length; i++) {
-  navOptions[i].addEventListener('mouseover', showDropdown);
-  navOptions[i].addEventListener('mouseout', reset);
-}
-
-dropdown.addEventListener('mouseover', showDropdown);
-dropdown.addEventListener('mouseout', reset);
-
-function myFunction(x) {
-  x.classList.toggle("change");
-  document.querySelector('.header-right-dropdown').classList.toggle("change");
+// Display hamburger dropdown when hamburger button clicked
+function displayHamburgerDropdown(hamburgerButton) {
+  hamburgerButton.classList.toggle("change");
+  document.querySelector('.hamburger-dropdown').classList.toggle("change");
 }
 
 // Cognitive Fusion Questionnaire Calculator
